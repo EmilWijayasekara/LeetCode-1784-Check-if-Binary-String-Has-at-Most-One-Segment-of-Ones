d829b5ade588cd9d5140bff1b81b03ddf3974d88
Emil <132345514+EmilWijayasekara@users.noreply.github.com>
2024-01-07 14:44:32 +0530

    Update 1784.check-if-binary-string-has-at-most-one-segment-of-ones.java

--- 1784.check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1784.check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,14 +1,6 @@
-/*
- * @lc app=leetcode id=1784 lang=java
- *
- * [1784] Check if Binary String Has at Most One Segment of Ones
- */
-
-// @lc code=start
 class Solution {
     public boolean checkOnesSegment(String s) {
         return !s.contains("01");
     }
 }
-// @lc code=end
-
+// https://github.com/EmilWijayasekara
